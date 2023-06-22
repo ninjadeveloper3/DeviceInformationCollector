5e372cb67b655c49528ce719bba0cb30cf4c71b9
DESKTOP-7MS7461\M.Adil <muhammad.adil@devbatch.com>
2023-06-22 11:44:45 +0500

    add functions in DataCollection.kt class for the library customization

--- deviceinformationlibrary/src/main/java/com/device/deviceinformationlibrary/DataCollection.kt
+++ deviceinformationlibrary/src/main/java/com/device/deviceinformationlibrary/DataCollection.kt
@@ -1,10 +1,16 @@
 package com.device.deviceinformationlibrary
 
+import android.accessibilityservice.AccessibilityServiceInfo
+import android.app.Activity
 import android.app.WallpaperManager
 import android.content.ContentResolver
 import android.content.Context
 import android.content.pm.PackageManager
 import android.content.res.Configuration
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
+import android.net.wifi.WifiInfo
+import android.net.wifi.WifiManager
 import android.os.Build
 import android.os.Environment
 import android.os.Process
@@ -12,12 +18,16 @@ import android.os.StatFs
 import android.provider.MediaStore
 import android.provider.Settings
 import android.util.Log
+import android.view.accessibility.AccessibilityManager
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import java.io.BufferedReader
-import java.io.File
-import java.io.FileReader
+import java.io.*
 import java.net.Inet4Address
 import java.net.NetworkInterface
+import java.text.DecimalFormat
+import java.util.*
+import java.util.regex.Matcher
+import java.util.regex.Pattern
 
 object DataCollection {
 
@@ -184,4 +194,256 @@ fun isDarkMode(context: Context): Boolean {
     val currentNightMode =
         context.resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK
     return currentNightMode == Configuration.UI_MODE_NIGHT_YES
+}
+
+// total internal storage of device
+fun getTotalInternalStorage(): Long {
+    val path = Environment.getDataDirectory()
+    val stat = StatFs(path.path)
+    val blockSize = stat.blockSizeLong
+    val totalBlocks = stat.blockCountLong
+    val totalInGb = totalBlocks * blockSize
+    val totalInternalStorage = totalInGb / (1024 * 1024 * 1024)
+    return totalInternalStorage
+
+}
+
+
+// get total external storage
+
+fun getTotalExternalStorageSizeInGB(): Long {
+    val externalStorageDirectory = Environment.getExternalStorageDirectory()
+    val statFs = StatFs(externalStorageDirectory.path)
+
+    val blockSize = statFs.blockSizeLong
+    val totalBlocks = statFs.blockCountLong
+
+    val totalBytes = blockSize * totalBlocks
+    val totalGB = totalBytes / (1024 * 1024 * 1024) // Convert bytes to GB
+
+    return totalGB
+}
+
+
+// this function  return the almost exact size
+// Ram size
+fun getTotalPhysicalMemory(): String? {
+    var reader: RandomAccessFile? = null
+    var load: String? = null
+    val twoDecimalForm = DecimalFormat("#.##")
+    var totRam = 0.0
+    var lastValue = ""
+    try {
+        reader = RandomAccessFile("/proc/meminfo", "r")
+        load = reader.readLine()
+
+        // Get the Number value from the string
+        val p: Pattern = Pattern.compile("(\\d+)")
+        val m: Matcher = p.matcher(load)
+        var value = ""
+
+        while (m.find()) {
+            value = m.group(1)
+            // System.out.println("Ram : " + value);
+        }
+
+        reader.close()
+        totRam = value.toDouble()
+        // totRam = totRam / 1024;
+        val mb = totRam / 1024.0
+        val gb = totRam / 1048576.0
+        val tb = totRam / 1073741824.0
+        lastValue = if (tb > 1) {
+            twoDecimalForm.format(tb) + (" TB")
+        } else if (gb > 1) {
+            twoDecimalForm.format(gb) + (" GB")
+        } else if (mb > 1) {
+            twoDecimalForm.format(mb) + (" MB")
+        } else {
+            twoDecimalForm.format(totRam) + (" KB")
+        }
+    } catch (ex: IOException) {
+        ex.printStackTrace()
+    } finally {
+        // Streams.close(reader);
+    }
+    return lastValue
+}
+
+// check Vpn is enabled or not
+fun isVpnEnabled(context: Context): Boolean {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+        val connectivityManager =
+            context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val activeNetwork = connectivityManager.activeNetwork
+        val networkCapabilities = connectivityManager.getNetworkCapabilities(activeNetwork)
+
+        return networkCapabilities?.hasTransport(NetworkCapabilities.TRANSPORT_VPN) ?: false
+    } else {
+        val connectivityService =
+            context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+        val iConnectivityManagerClass = Class.forName(connectivityService.javaClass.name)
+        val iConnectivityManagerField = iConnectivityManagerClass.getDeclaredField("mService")
+        iConnectivityManagerField.isAccessible = true
+        val iConnectivityManager = iConnectivityManagerField.get(connectivityService)
+        val iConnectivityManagerClassStub = Class.forName(iConnectivityManager.javaClass.name)
+        val getVpnConfigMethod = iConnectivityManagerClassStub.getDeclaredMethod("getVpnConfig")
+        getVpnConfigMethod.isAccessible = true
+        val vpnConfig = getVpnConfigMethod.invoke(iConnectivityManager)
+        return vpnConfig != null
+    }
+}
+
+// get screen time off
+fun getScreenOffTimeout(context: Context): Long {
+    return try {
+        // Get the current screen timeout duration
+        val timeout = Settings.System.getLong(
+            context.contentResolver,
+            Settings.System.SCREEN_OFF_TIMEOUT
+        )
+        // Convert milliseconds to seconds
+        timeout / 1000
+    } catch (e: Exception) {
+        e.printStackTrace()
+        // Handle exception if necessary
+        -1L
+    }
+}
+
+// check if devices rooted or not
+fun isRooted(): Boolean {
+    val path = System.getenv("PATH")
+    val places = path.split(":")
+    for (place in places) {
+        val file = File(place, "su")
+        if (file.exists()) {
+            return true
+        }
+    }
+    return false
+}
+
+// second Method to check that device is rooted or not
+// these for some device whose not like that regular devices
+//Please note that these methods are not foolproof,
+//as rooted devices can use various techniques to hide their status.
+//These approaches can provide a preliminary indication, but it's not guaranteed to be 100% accurate.
+fun isDeviceRooted(): Boolean {
+    val knownRootFileSystems =
+        arrayListOf("/sbin/su", "/system/su", "/system/bin/su", "/system/xbin/su")
+    for (filePath in knownRootFileSystems) {
+        val file = File(filePath)
+        if (file.exists()) {
+            return true
+        }
+    }
+    return false
+}
+
+// number of cores of the device cpu
+fun getNumberOfCores(): Int {
+    return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+        Runtime.getRuntime().availableProcessors()
+    } else {
+        // For older devices, use a different method to get the number of cores
+        try {
+            val file = File("/sys/devices/system/cpu/")
+            val files = file.listFiles { _, name -> name.startsWith("cpu") }
+            files.size
+        } catch (e: Exception) {
+            Runtime.getRuntime().availableProcessors()
+            Log.d("exceptionMessage", e.message.toString())
+        }
+    }
+}
+
+//indicate device is possible emulator or not
+
+fun isEmulator(): Boolean {
+    return (Build.FINGERPRINT.contains("generic")
+            || Build.MODEL.contains("google_sdk")
+            || Build.MODEL.contains("Emulator")
+            || Build.MODEL.contains("Android SDK built for x86")
+            || Build.MANUFACTURER.contains("Genymotion")
+            || (Build.BRAND.startsWith("generic") && Build.DEVICE.startsWith("generic"))
+            || "google_sdk" == Build.PRODUCT)
+}
+
+
+// get device timezone offset
+fun getDeviceTimeZoneOffset(): Int {
+    val currentTimeZone = TimeZone.getDefault()
+    val offsetInMillis = currentTimeZone.getOffset(Calendar.getInstance().timeInMillis)
+    // convert offsetInMillis to hours
+    return offsetInMillis / (1000 * 60 * 60)
+}
+
+// Function to check if Auto Net selection is enabled
+fun isAutoNetSelectionEnabled(context: Context): Boolean {
+    val connectivityManager =
+        context.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+
+    // Check if the device is running on Android Q or higher
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+        val networkCapabilities =
+            connectivityManager.getNetworkCapabilities(connectivityManager.activeNetwork)
+        return networkCapabilities?.hasCapability(NetworkCapabilities.NET_CAPABILITY_NOT_METERED) == true
+    }
+
+    // Auto Net selection not applicable for Android versions below Q
+    return false
+}
+
+// check permission function
+// Check if the app has the necessary permissions to access the Wi-Fi state
+private fun hasPermission(context: Activity, permission: String): Boolean {
+    return ContextCompat.checkSelfPermission(
+        context,
+        permission
+    ) == PackageManager.PERMISSION_GRANTED
+}
+
+// Get the MAC address of the Wi-Fi hardware
+fun getMacAddress(context: Context): String? {
+    val wifiManager =
+        context.applicationContext.getSystemService(Context.WIFI_SERVICE) as WifiManager
+
+    // Check if the app has the necessary permissions
+    if (hasPermission(context as Activity, android.Manifest.permission.ACCESS_WIFI_STATE)) {
+        val wifiInfo: WifiInfo? = wifiManager.connectionInfo
+        if (wifiInfo != null) {
+            return wifiInfo.macAddress
+        }
+    } else {
+        // Request the necessary permissions if not granted
+        ActivityCompat.requestPermissions(
+            context,
+            arrayOf(android.Manifest.permission.ACCESS_WIFI_STATE),
+            1
+        )
+    }
+
+    return null
+}
+
+// check closed captioning enabled or not
+fun isClosedCaptioningEnabled(context: Context): Boolean {
+    val captioningManager =
+        context.getSystemService(Context.CAPTIONING_SERVICE) as android.view.accessibility.CaptioningManager
+
+    return captioningManager.isEnabled
+}
+
+
+// check talk back option enabled or not
+fun Context.isScreenReaderOn(): Boolean {
+    val am = getSystemService(Context.ACCESSIBILITY_SERVICE) as AccessibilityManager
+    if (am != null && am.isEnabled) {
+        val serviceInfoList =
+            am.getEnabledAccessibilityServiceList(AccessibilityServiceInfo.FEEDBACK_SPOKEN)
+        if (!serviceInfoList.isEmpty())
+            return true
+    }
+    return false
 }
